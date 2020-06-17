68e467f475e51b7329c24c4eb6198f6ecfcaeffc
Kurt Kim <kurtsh.kim@gmail.com>
2020-06-17 13:22:46 -0400

    - implementing Core Data

--- MLBPlayerSearch/MLBPlayerSearch/AppDelegate.swift
+++ MLBPlayerSearch/MLBPlayerSearch/AppDelegate.swift
@@ -7,10 +7,34 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "FavoritePlayerCoreData")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+//    MARK: - Core Data Saving support
+    func saveContext(){
+        let context = persistentContainer.viewContext
+        if context.hasChanges{
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
     var window: UIWindow?
 
 
---
+++ MLBPlayerSearch/MLBPlayerSearch/CoreData/CoreDataHandler.swift
@@ -0,0 +1,14 @@
+//
+//  CoreDataHandler.swift
+//  MLBPlayerSearch
+//
+//  Created by Kurt Kim on 2020-06-17.
+//  Copyright © 2020 KK. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+
+
